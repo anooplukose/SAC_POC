b2d735bec2355332edf946c55ab670f8bfab9766
anooplukose <67674247+anooplukose@users.noreply.github.com>
2022-09-02 19:19:26 +0530

    Update HW_Test.js

--- HW_Test.js
+++ HW_Test.js
@@ -1,4 +1,4 @@
-console.log("16");
+console.log("18");
 (function()  {
 	 const echart1js = "https://fastly.jsdelivr.net/npm/jquery";
     const echart2js = "https://fastly.jsdelivr.net/npm/echarts@5/dist/echarts.min.js";
@@ -40,6 +40,12 @@ console.log("22");
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(template.content.cloneNode(true));
             this._firstConnection = false;
+			
+			 this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+            });
+			console.log("aa");
 		}
 
         //Fired when the widget is added to the html DOM of the page
@@ -108,88 +114,6 @@ console.log("22");
        console.log("Step-11");
         }
 	    
-	     const countries = [
-    'Finland',
-    'France',
-    'Germany',
-    'Iceland',
-    'Norway',
-    'Poland',
-    'Russia',
-    'United Kingdom'
-  ];
-  const datasetWithFilters = [];
-  const seriesList = [];
-  echarts.util.each(countries, function (country) {
-    var datasetId = 'dataset_' + country;
-    datasetWithFilters.push({
-      id: datasetId,
-      fromDatasetId: 'dataset_raw',
-      transform: {
-        type: 'filter',
-        config: {
-          and: [
-            { dimension: 'Year', gte: 1950 },
-            { dimension: 'Country', '=': country }
-          ]
-        }
-      }
-    });
-    seriesList.push({
-      type: 'line',
-      datasetId: datasetId,
-      showSymbol: false,
-      name: country,
-      endLabel: {
-        show: true,
-        formatter: function (params) {
-          return params.value[3] + ': ' + params.value[0];
-        }
-      },
-      labelLayout: {
-        moveOverlap: 'shiftY'
-      },
-      emphasis: {
-        focus: 'series'
-      },
-      encode: {
-        x: 'Year',
-        y: 'Income',
-        label: ['Country', 'Income'],
-        itemName: 'Year',
-        tooltip: ['Income']
-      }
-    });
-  });
-  option = {
-    animationDuration: 10000,
-    dataset: [
-      {
-        id: 'dataset_raw',
-        source: _rawData
-      },
-      ...datasetWithFilters
-    ],
-    title: {
-      text: 'Income of Germany and France since 1950'
-    },
-    tooltip: {
-      order: 'valueDesc',
-      trigger: 'axis'
-    },
-    xAxis: {
-      type: 'category',
-      nameLocation: 'middle'
-    },
-    yAxis: {
-      name: 'Income'
-    },
-    grid: {
-      right: 140
-    },
-    series: seriesList
-  };
-  myChart.setOption(option);
-}
+
     });
 })();
