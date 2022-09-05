4c0c902e702799b499472ca9aea9bdf897e3ad45
anooplukose <67674247+anooplukose@users.noreply.github.com>
2022-09-05 15:47:45 +0530

    Update echarts4.js

--- echarts4.js
+++ echarts4.js
@@ -8,7 +8,7 @@ var getScriptPromisify = (src) => {
   
    const echart1js = "https://fastly.jsdelivr.net/npm/jquery";
     const echart2js = "https://fastly.jsdelivr.net/npm/echarts@5/dist/echarts.min.js";
-	console.log("26");
+	console.log("27");
 	function loadScript(src)
     {
     
