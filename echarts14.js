93b3f6fbbef0b080c55ee689f89650bfc79a57a8
anooplukose <67674247+anooplukose@users.noreply.github.com>
2022-09-10 11:22:24 +0530

    Add files via upload

---
+++ echarts14.js
@@ -0,0 +1,144 @@
+var getScriptPromisify = (src) => {
+  return new Promise(resolve => {
+    $.getScript(src, resolve)
+  })
+}
+
+(function () {
+  const prepared = document.createElement('template')
+  prepared.innerHTML = `
+     <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `
+  class SamplePrepared14 extends HTMLElement {
+    constructor () {
+      super()
+console.log("23")
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+
+      this._root = this._shadowRoot.getElementById('root')
+
+      this._props = {}
+
+     this.render()
+    }
+
+  onCustomWidgetResize (width, height) {
+      this.render()
+    }
+
+    async render () {
+     // await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+await getScriptPromisify('https://fastly.jsdelivr.net/npm/jquery')
+       await getScriptPromisify('https://fastly.jsdelivr.net/npm/echarts@5/dist/echarts.min.js')
+
+    
+      const myChart = echarts.init(this._root)
+      const countries = [
+    'China',
+    'Australia',
+    'Canada',
+     'Finland',
+        'Cuba',
+        'France'
+    
+  ];
+  const datasetWithFilters = [];
+  const seriesList = [];
+  echarts.util.each(countries, function (country) {
+    var datasetId = country;
+    datasetWithFilters.push({
+      id: datasetId,
+    
+      transform: {
+        type: 'filter',
+        config: {
+          and: [
+            { dimension: 'Year', gte: 1950 },
+            { dimension: 'Country', '=': country }
+          ]
+        }
+      }
+    });
+    seriesList.push({
+      type: 'line',
+      datasetId: datasetId,
+      showSymbol: false,
+      name: country,
+      endLabel: {
+        show: true,
+        formatter: function (params) {
+          return params.value[0] + ': ' + params.value[2];
+        }
+      },
+      labelLayout: {
+        moveOverlap: 'shiftY'
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      encode: {
+        x: 'Year',
+        y: 'Income',
+        label: ['Country', 'Income'],
+        itemName: 'Year',
+        tooltip: ['Income']
+      }
+    });
+  });
+ const option = {
+    animationDuration: 10000,
+    dataset: [
+      {
+    source: [
+      ['Country','Year','Income'],
+['China','2000',1516],
+['China','2010',9430],
+['China','2015',13334],
+['Australia','2000',35253],
+['Australia','2010',41330],
+['Australia','2015',44056],
+['Canada','2000',37314],
+['Canada','2010',40773],
+['Canada','2015',43294],
+['Finland','2000',34517],
+['Finland','2010',39425],
+['Finland','2015',38923],
+['Cuba','2000',11268],
+['Cuba','2010',18477],
+['Cuba','2015',21291],
+['France','2000',34774],
+['France','2010',36745],
+['France','2015',37599]
+    ]
+  },
+      ...datasetWithFilters
+    ],
+    title: {
+      text: 'Income of Germany and France since 1950'
+    },
+    tooltip: {
+      order: 'valueDesc',
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'category',
+      nameLocation: 'middle'
+    },
+    yAxis: {
+      name: 'Income'
+    },
+    grid: {
+      right: 140
+    },
+    series: seriesList
+  };
+      myChart.setOption(option)
+    }
+  }
+
+  customElements.define('com-sap-sample-echarts-prepared14', SamplePrepared14)
+})()
