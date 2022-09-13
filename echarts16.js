aeaa276b0e0c6b220696c55e57c3f364c89caeb6
anooplukose <67674247+anooplukose@users.noreply.github.com>
2022-09-13 14:13:06 +0530

    Update echarts16.js

--- echarts16.js
+++ echarts16.js
@@ -24,7 +24,7 @@ var getScriptPromisify = (src) => {
   class SamplePrepared16 extends HTMLElement {
     constructor () {
       super()
-      console.log("019")
+      console.log("021")
       this._shadowRoot = this.attachShadow({ mode: 'open' })
       this._shadowRoot.appendChild(template.content.cloneNode(true))
       this._root = this._shadowRoot.getElementById('root')
@@ -88,9 +88,9 @@ this._placeholder = this._root.querySelector('#placeholder')
 		    const { rawValue, description } = dp[MEASURE_DIMENSION]
 		    const country = dp.Region.description
 		    const year = Number(dp.Year.description)
-		    
+		    const rawNo= Number(rawValue)
 		   
-		    const arr= [year,country,rawValue]
+		    const arr= [year,country,rawNo]
 		    
 		    if (countries.indexOf(country) === -1) {
           countries.push(country)
