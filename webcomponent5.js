dfeff99209e5951009f9d09b1833191ae331271b
anooplukose <67674247+anooplukose@users.noreply.github.com>
2022-09-22 14:15:43 +0530

    Update webcomponent5.js

--- webcomponent5.js
+++ webcomponent5.js
@@ -62,7 +62,7 @@
 	
    <body>
         <div class="marquee">
-            <p> Top selling product for 2021 is THIAMETHOXAM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Top selling product family for 2021 is Fungicides&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Max revenue generating region for 2021 from overall sales is Latin America with $4.5b&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Max revenue generating country from all regions for 2021 from overall sales is Brazil South & East /Seeds BR South $1.2b   </p>
+            Top selling product for 2021 is THIAMETHOXAM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Top selling product family for 2021 is Fungicides&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Max revenue generating region for 2021 from overall sales is Latin America with $4.5b&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Max revenue generating country from all regions for 2021 from overall sales is Brazil South & East /Seeds BR South $1.2b   
         </div>
     </body>
     `;
@@ -71,7 +71,7 @@
 
 
 		constructor() {
-			console.log("010")
+			console.log("001")
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
